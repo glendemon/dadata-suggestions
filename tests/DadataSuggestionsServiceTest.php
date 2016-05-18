05cecb0a0953cd9fc54ba627fc7ccf9e8d60c9f7
Victor Demin <mail@vdemin.com>
2016-05-18 16:58:31 +0400

    test address with flat

--- tests/DadataSuggestionsServiceTest.php
+++ tests/DadataSuggestionsServiceTest.php
@@ -21,6 +21,29 @@ class DadataSuggestionsServiceTest extends PHPUnit_Framework_TestCase
         }
     }
 
+    public function testSuggestAddressFlat()
+    {
+        $service = $this->getService();
+        $response = $service->suggestAddress('мск балтийская 6к1 5');
+        $this->assertNotEmpty($response);
+        $this->assertInstanceOf(\DadataSuggestions\Response::class, $response);
+        $this->assertNotEmpty($response->getSuggestions());
+        $this->assertInternalType('array', $response->getSuggestions());
+        foreach ($response->getSuggestions() as $suggestion) {
+            $this->assertEquals('г Москва, ул Балтийская, д 6 к 1, кв 5', $suggestion->getValue());
+            $this->assertEquals('г Москва, ул Балтийская, д 6 к 1, кв 5', $suggestion->getUnrestrictedValue());
+            /** @var \DadataSuggestions\Data\Address $data */
+            $data = $suggestion->getData();
+            $this->assertInstanceOf(\DadataSuggestions\Data\Address::class, $data);
+            $this->assertEquals('Россия', $data->country);
+            $this->assertEquals('Москва', $data->city);
+            $this->assertEquals('ул Балтийская', $data->street_with_type);
+            $this->assertEquals('6', $data->house);
+            $this->assertEquals('1', $data->block);
+            $this->assertEquals('5', $data->flat);
+        }
+    }
+
     /**
      * @return \DadataSuggestions\DadataSuggestionsService
      */
