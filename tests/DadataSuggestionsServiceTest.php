619f1341ffd6e73c6c1343bf721f50bd367b5755
Victor Demin <mail@vdemin.com>
2016-05-18 16:06:44 +0400

    update test

--- tests/DadataSuggestionsServiceTest.php
+++ tests/DadataSuggestionsServiceTest.php
@@ -2,13 +2,28 @@
 
 class DadataSuggestionsServiceTest extends PHPUnit_Framework_TestCase
 {
-    public function testSome()
+    public function testSuggestAddressSimple()
     {
-        $service = new \DadataSuggestions\DadataSuggestionsService();
-        $response = $service->suggestFio('Викто');
+        $service = $this->getService();
+        $response = $service->suggestAddress('moskv');
         $this->assertNotEmpty($response);
         $this->assertInstanceOf(\DadataSuggestions\Response::class, $response);
-        $response->getValue();
+        $this->assertNotEmpty($response->getSuggestions());
+        $this->assertInternalType('array', $response->getSuggestions());
+        foreach ($response->getSuggestions() as $suggestion) {
+            $this->assertEquals('г Москва', $suggestion->getValue());
+            $this->assertEquals('г Москва', $suggestion->getUnrestrictedValue());
+        }
+    }
+
+    /**
+     * @return \DadataSuggestions\DadataSuggestionsService
+     */
+    protected function getService()
+    {
+        $service = new \DadataSuggestions\DadataSuggestionsService();
+        $service->setToken('ab626ac37b47868748ea2f408293ed6a1e420944');
+        return $service;
     }
 
 }
